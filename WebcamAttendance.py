a767df10e4e78821f7621c0082c63462f5b57a19
mattweedy <matthewtweedy02@gmail.com>
2025-01-29 14:02:15 +1100

    Name and time of appearance recorded in attendance logs

--- WebcamAttendance.py
+++ WebcamAttendance.py
@@ -1,7 +1,8 @@
+import os
 import cv2
 import numpy as np
 import face_recognition
-import os
+from datetime import datetime
 
 # TODO: implement multithreading to increase performance
 
@@ -42,7 +43,21 @@ def drawRect(img, name, x1, y1, x2, y2):
     cv2.rectangle(img, (x1, y1), (x2, y2), (rectB, rectG, rectR), 2)
     cv2.rectangle(img, (x1, y2-35), (x2, y2), (rectB, rectG, rectR), cv2.FILLED)
     cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.6, (nameB, nameG, nameR), 2)
-
+    
+def markAttendance(name):
+    # TODO: expand functionality here, get creative
+    with open('Logs/Attendance.csv', 'r+') as file:
+        dataList = file.readlines()
+        nameList = []
+        for line in dataList:
+            entry = line.split(',')
+            # entry[0] is the name
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            file.writelines(f'\n{name},{dtString}')
+        
 encodeListKnownFaces = findEncodings(images)
 print('Encoding Complete') # debug
 
@@ -64,6 +79,7 @@ while True:
     
     # finding matches
     # compare all faces to all known faces (encodings)
+    # TODO: extract this to a function
     for encodeFace, faceLoc in zip(encodeCurrFrame, facesCurrFrame):
         # compare current encodeFace to all known faces
         matches = face_recognition.compare_faces(encodeListKnownFaces, encodeFace)
@@ -76,10 +92,12 @@ while True:
         # print the name of the match if it exists
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-            print(f'match found : {name}')
+            markAttendance(name)
+            # print(f'match found : {name}')
         else:
             name = 'Unknown'
-            print(f'match found : {name}')
+            markAttendance(name)
+            # print(f'match found : {name}')
 
         # draw rectangle around face
         y1, x2, y2, x1 = faceLoc
