edb4591d50fbbd217ca9a44ac63bdbf457c15016
mattweedy <matthewtweedy02@gmail.com>
2025-01-29 16:57:46 +1100

    Removed frame skipping, neglible performance increase and led to bad UE

--- FrameProcessor.py
+++ FrameProcessor.py
@@ -4,15 +4,16 @@ import face_recognition
 from threading import Thread
 
 class FrameProcessor:
-    def __init__(self, video_grabber, video_shower, cps, compareFacesFn, skip_frames=5):
+    # def __init__(self, video_grabber, video_shower, cps, compareFacesFn, skip_frames=5):
+    def __init__(self, video_grabber, video_shower, cps, compareFacesFn):
         self.grabber = video_grabber
         self.shower = video_shower
         self.cps = cps
         self.compareFaces = compareFacesFn
         self.stopped = False
         self.thread = None
-        self.skip_frames = skip_frames
-        self.frame_counter = 0
+        # self.skip_frames = skip_frames
+        # self.frame_counter = 0
 
     def start(self):
         self.thread = Thread(target=self.process_frames, args=())
@@ -26,18 +27,23 @@ class FrameProcessor:
             
             frame = self.grabber.frame
             
+            imgSmall = cv2.resize(frame, (0,0), None, 0.25, 0.25)
+            imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
+            facesCurrFrame = face_recognition.face_locations(imgSmall)
+            encodeCurrFrame = face_recognition.face_encodings(imgSmall, facesCurrFrame)
+            self.compareFaces(encodeCurrFrame, facesCurrFrame, frame)
             # process faces every skip_frames frames
             # for better performance
-            if self.frame_counter % self.skip_frames == 0:
-                imgSmall = cv2.resize(frame, (0,0), None, 0.25, 0.25)
-                imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
-                facesCurrFrame = face_recognition.face_locations(imgSmall)
-                encodeCurrFrame = face_recognition.face_encodings(imgSmall, facesCurrFrame)
-                self.compareFaces(encodeCurrFrame, facesCurrFrame, frame)
+            # if self.frame_counter % self.skip_frames == 0:
+            #     imgSmall = cv2.resize(frame, (0,0), None, 0.25, 0.25)
+            #     imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
+            #     facesCurrFrame = face_recognition.face_locations(imgSmall)
+            #     encodeCurrFrame = face_recognition.face_encodings(imgSmall, facesCurrFrame)
+            #     self.compareFaces(encodeCurrFrame, facesCurrFrame, frame)
             
             self.cps.increment()
             self.shower.frame = frame
-            self.frame_counter += 1
+            # self.frame_counter += 1
 
     def stop(self):
         self.stopped = True
