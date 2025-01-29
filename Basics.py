9ee8415c104f5676e35002884499d651184a35f1
mattweedy <matthewtweedy02@gmail.com>
2025-01-29 14:02:13 +1100

    Added debug output onto test img

--- Basics.py
+++ Basics.py
@@ -8,8 +8,9 @@ imgLeo = face_recognition.load_image_file('ImagesBasic/LeonardoDiCaprio.jpg')
 imgLeo = cv2.cvtColor(imgLeo, cv2.COLOR_BGR2RGB)
 
 # import test image
-imgLeoTest = face_recognition.load_image_file('ImagesBasic/Leonardo_Test.jpg')
-imgLeoTest = cv2.cvtColor(imgLeoTest, cv2.COLOR_BGR2RGB)
+# imgTest = face_recognition.load_image_file('ImagesBasic/Leonardo_Test.jpg')
+imgTest = face_recognition.load_image_file('ImagesBasic/BarackObama.jpg')
+imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
 
 # locate faces
 faceLoc = face_recognition.face_locations(imgLeo)[0]
@@ -20,17 +21,24 @@ encodeLeo = face_recognition.face_encodings(imgLeo)[0]
 # draw from top left to bottom right
 cv2.rectangle(imgLeo, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (0, 245, 112), 2)
 
-faceLocTest = face_recognition.face_locations(imgLeoTest)[0]
-encodeLeoTest = face_recognition.face_encodings(imgLeoTest)[0]
-cv2.rectangle(imgLeoTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (0, 245, 112), 2)
+faceLocTest = face_recognition.face_locations(imgTest)[0]
+encodeTest = face_recognition.face_encodings(imgTest)[0]
+cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (0, 245, 112), 2)
 
 # comparing faces and finding the distance between them
 # comparing the 128 encodings
-results = face_recognition.compare_faces([encodeLeo], encodeLeoTest)
-print(results[0])
+results = face_recognition.compare_faces([encodeLeo], encodeTest)
+# the lower the dist, the better the match
+faceDis = face_recognition.face_distance([encodeLeo], encodeTest)
+print(results[0], faceDis)
+# cv2.putText(imgTest, f'{results} {round(faceDis[0],2)}', (50,50), cv2.FONT_HERSHEY_COMPLEX,0.7,(0,255,200),2)
+if faceDis < 0.6:
+    cv2.putText(imgTest, f'{results[0]} {round(faceDis[0],2)}', (faceLocTest[3], faceLocTest[2]), cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,255,0),2)
+else:
+    cv2.putText(imgTest, f'{results[0]} {round(faceDis[0],2)}', (faceLocTest[3], faceLocTest[2]), cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,0,255),2)
 
 # display images
 cv2.imshow('Leo', imgLeo)
-cv2.imshow('LeoTest', imgLeoTest)
+cv2.imshow('LeoTest', imgTest)
 cv2.waitKey(0)
 
